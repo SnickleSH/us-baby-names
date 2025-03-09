cabd2949877348cc79fbef977f45967246811680
Kristóf Németh <138215045+SnickleSH@users.noreply.github.com>
2025-03-09 16:54:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+ # US Baby Names Trend Analysis
+
+ https://app.mavenanalytics.io/guided-projects/f71c0a2b-05f4-43fe-a80c-8f3f86964ccc
---
+++ us-baby-names.sql
@@ -0,0 +1,25 @@
+-- Objective 1: Track changes in name popularity
+
+-- Task 1: Find the overall most popular girl and boy names and show how they have changed in popularity rankings over the years
+
+-- Task 2: Find the names with the biggest jumps in popularity from the first year of the data set to the last year
+
+-- Objective 2: Compare popularity across decades
+
+-- Task 1: For each year, return the 3 most popular girl names and 3 most popular boy names
+
+-- Task 2: For each decade, return the 3 most popular girl names and 3 most popular boy names
+
+-- Objective 3: Compare popularity across regions
+
+-- Task 1: Return the number of babies born in each of the six regions (NOTE: The state of MI should be in the Midwest region)
+
+-- Task 2: Return the 3 most popular girl names and 3 most popular boy names within each region
+
+-- Objective 4: Explore unique names in the dataset
+
+-- Task 1: Find the 10 most popular androgynous names (names given to both females and males)
+
+-- Task 2: Find the length of the shortest and longest names, and identify the most popular short names (those with the fewest characters) and long names (those with the most characters)
+
+-- Task 3: The founder of Maven Analytics is named Chris. Find the state with the highest percent of babies named "Chris"
